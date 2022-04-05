e71a3a15dfb71531923bcb6e892f003048eae093
Paulo Vítor <paulovmedeiros@outlook.com>
2022-04-05 11:15:04 -0300

    exercicio 1 - criando app

---
+++ microsoft docs/exercise 1/index.js
@@ -0,0 +1,12 @@
+const app = Vue.createApp({
+    
+    data() {
+        // data é o argumento que vai ser passado para o componente Apps
+        return {
+            productName: 'Book a Cruise to the Moon',
+            productDescription: 'Cruise to the moon in our luxurious shuttle. Watch the astronauts working outside the International Space Station.',
+
+            // Adicionar mais informações...
+        }
+    },
+});
